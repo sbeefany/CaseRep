fd4f1ab5fe9d38906aa0a2d4895a3cfa0e5d67ef
Egor Alexsandrov <e.aleksandrov@it-cron.ru>
2020-12-05 21:44:13 +0300

    #25 #26

--- frontend/src/Projects/components/Project/components/Tasks/Tasks.jsx
+++ frontend/src/Projects/components/Project/components/Tasks/Tasks.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
-import s from './Tasks.module.css'
+import s from './Tasks.module.css';
+import edit from '../../../../../img/edit.jpg'
 
 const Tasks = (props) => {
 
     const { tasks, allWorkers,title,position,setNewTask } = props;
+
+    const [isEditMode, setEditMode] = useState(-1)
 
     return (
         <>
@@ -21,6 +24,7 @@ const Tasks = (props) => {
                         <td className={s.td1}>№</td>
                         <td className={s.td2}>Название</td>
                         <td className={s.td3}>Статус</td>
+                        <td className={s.td3}>Выполнить до</td>
                         <td className={s.td3}>Исполнитель</td>
                         <td className={s.td3}>Баллы</td>
                         <td className={s.td4}></td>
@@ -32,12 +36,15 @@ const Tasks = (props) => {
                         return (
                             <tr className={s.tr}>
                                 <td className={s.td1}>{key + 1}</td>
-                                <td className={s.td2}>{elem.title}</td>
-                                <td className={s.td3}>{elem.status === 1 ? 'Выполнено' : 'Не выполнено'}</td>
+                                <td className={s.td2}> {isEditMode === elem.id && position==2 ? <input className={s.input} value={elem.title}/>:elem.title}</td>
+                                <td className={s.td3}>{isEditMode === elem.id ? <select><option>Выполнено</option><option> Не выполнено</option></select> : elem.status === 1 ? 'Выполнено' : 'Не выполнено'}</td>
+                                <td className={s.td3}>{isEditMode === elem.id && position==2 ? <input className={s.input} value={elem.theEndDate.slice(0,10)}/> : elem.theEndDate.slice(0,10)}</td>
                                 <td className={s.td3}>{allWorkers.find(worker => worker.id === +elem.authorId).name} {' '}
                                     {allWorkers.find(worker => worker.id === +elem.authorId).surename}</td>
-                                <td className={s.td3}>{elem.weight}</td>
-                                <td className={s.td4}></td>
+                                <td className={s.td3}>{isEditMode  === elem.id && position===2 ? <input className={s.input} value={elem.weight}/> : elem.weight}</td>
+                        <td className={s.td4}>{((title === 'Мои задачи' && position === 3) || (title ==='Задачи проекта' && position===2)) ?
+                          isEditMode === -1 ? 
+                        <img onClick={()=>setEditMode(elem.id)} className={s.editImg} src={edit}/>: <div onClick={()=>setEditMode(-1)}>Сохр</div> : ''}</td>
                                 <td className={s.td4}></td>
                             </tr>)
                     })}
